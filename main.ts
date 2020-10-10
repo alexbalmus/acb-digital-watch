e612bf77ac3083d1d9fcba11d228ba4194bd974f
Alexandru-Cristian Balmus <alexbalmus@gmail.com>
2020-10-10 16:12:15 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,106 @@
-let hours = 0
-let ampm = false
-let minutes = 0
-let adjust = 0
-let time = ""
+let hours = 0;
+let ampm = false;
+let minutes = 0;
+let adjust = 0;
+let time = "";
 
-input.onButtonPressed(Button.A, function () {
-    if (hours < 23) {
-        hours += 1
-    } else {
-        hours = 0
+function btnAHandler() 
+{
+    if (hours < 23) 
+    {
+        hours++;
+    } 
+    else 
+    {
+        hours = 0;
     }
-})
+}
 
-input.onButtonPressed(Button.AB, function () {
-    ampm = !(ampm)
-})
+input.onButtonPressed(Button.A, btnAHandler);
 
-input.onButtonPressed(Button.B, function () {
-    if (minutes < 59) {
-        minutes += 1
-    } else {
-        minutes = 0
+function btnABHandler() 
+{
+    ampm = !(ampm);
+}
+
+input.onButtonPressed(Button.AB, btnABHandler);
+
+function btnBHandler() 
+{
+    if (minutes < 59) 
+    {
+        minutes++;
+    } 
+    else 
+    {
+        minutes = 0;
     }
-})
+}
 
-input.onGesture(Gesture.Shake, function () {
-    let hours2 = 0
-    adjust = hours2
+input.onButtonPressed(Button.B, btnBHandler);
 
-    if (ampm) {
-        if (hours > 12) {
-            adjust = hours - 12
-        } else {
-            if (hours == 0) {
-                adjust = 12
+function shakeHandler() 
+{
+    let hours2 = 0;
+    adjust = hours2;
+
+    if (ampm) 
+    {
+        if (hours > 12) 
+        {
+            adjust = hours - 12;
+        } 
+        else 
+        {
+            if (hours == 0) 
+            {
+                adjust = 12;
             }
         }
     }
 
-    time = "" + adjust
-    time = "" + time + ":"
-    time = "" + time + minutes / 10
-    time = "" + time + minutes % 10
+    time = "" + adjust;
+    time = "" + time + ":";
+    time = "" + time + minutes / 10;
+    time = "" + time + minutes % 10;
 
-    if (ampm) {
-        if (hours > 11) {
-            time = "" + time + "PM"
-        } else {
-            time = "" + time + "AM"
+    if (ampm) 
+    {
+        if (hours > 11) 
+        {
+            time = "" + time + "PM";
+        } 
+        else 
+        {
+            time = "" + time + "AM";
         }
     }
 
-    basic.showString(time)
-})
+    basic.showString(time);
+}
 
-basic.forever(function () {
-    
-    basic.pause(60000)
+input.onGesture(Gesture.Shake, shakeHandler);
 
-    if (minutes < 59) {
-        minutes += 1
-    } else {
-        minutes = 0
+function main() 
+{
+    basic.pause(60000);
+
+    if (minutes < 59) 
+    {
+        minutes++;
+    } 
+    else 
+    {
+        minutes = 0;
         
-        if (hours < 23) {
-            hours += 1
-        } else {
-            hours = 0
+        if (hours < 23) 
+        {
+            hours++;
+        } else 
+        {
+            hours = 0;
         }
     }
-})
+}
+
+basic.forever(main);
